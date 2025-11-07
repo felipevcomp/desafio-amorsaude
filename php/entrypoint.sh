a58152680fb4e74532e004223c7cc8d55183f706
felipevcomp <felipev.comp@gmail.com>
2025-11-06 22:45:26 -0300

    chore(docker): Docker environment configuration

---
+++ php/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+chown -R www-data:www-data /var/www/html/storage
+chown -R www-data:www-data /var/www/html/bootstrap/cache
+
+chmod -R 775 /var/www/html/storage
+chmod -R 775 /var/www/html/bootstrap/cache
+
+exec "$@"
