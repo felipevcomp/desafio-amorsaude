0d8cdfd0ed6c1ed30cb8eb94802cb8cf9cbf05cb
felipevcomp <felipev.comp@gmail.com>
2025-11-24 20:28:10 -0300

    chore(system): entrypoint.sh update

--- php/entrypoint.sh
+++ php/entrypoint.sh
@@ -7,16 +7,20 @@ chown -R www-data:www-data /var/www/html/bootstrap/cache
 chmod -R 775 /var/www/html/storage
 chmod -R 775 /var/www/html/bootstrap/cache
 
-# Espera o banco ficar disponível
-until php artisan migrate:status > /dev/null 2>&1; do
-  echo "Banco ainda não disponível. Tentando novamente em 3 segundos..."
+# Aguarda o banco subir
+echo "Aguardando MySQL..."
+until php -r "try { new PDO('mysql:host=${DB_HOST};dbname=${DB_DATABASE}', '${DB_USERNAME}', '${DB_PASSWORD}'); } catch (Exception \$e) { exit(1); }"; do
   sleep 3
 done
 
-# Executa migrations e seeders
-php artisan migrate --seed --force
+# Verifica se já existem usuários
+USER_COUNT=$(php artisan tinker --execute="echo DB::table('users')->count();")
 
-php artisan passport:install
+if [ "$USER_COUNT" -eq "0" ]; then
+    echo "Nenhum dado encontrado. Rodando migrations + seeders..."
+    php artisan migrate:fresh --seed
+else
+    echo "Seeders já rodaram anteriormente. Pulando..."
+fi
 
-# Por fim, executa o comando padrão (por exemplo: apache2-foreground)
 exec "$@"
