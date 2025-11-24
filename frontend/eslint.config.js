a845051d18d39160a02705cfd8957a41ab13bcf3
felipevcomp <felipev.comp@gmail.com>
2025-11-24 20:32:36 -0300

    chore(system): eslint configs

---
+++ frontend/eslint.config.js
@@ -0,0 +1,91 @@
+// ESLint 9 + Flat Config for Angular 17
+
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import angular from "@angular-eslint/eslint-plugin";
+import angularTemplate from "@angular-eslint/eslint-plugin-template";
+import importPlugin from "eslint-plugin-import";
+import jsdoc from "eslint-plugin-jsdoc";
+
+export default [
+  // Global ignores
+  {
+    ignores: [
+      ".angular/**/*",
+      "dist/**/*",
+      "node_modules/**/*"
+    ]
+  },
+
+  // TypeScript Rules
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        sourceType: "module",
+        ecmaVersion: 2022
+      }
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      "@angular-eslint": angular,
+      import: importPlugin,
+      jsdoc
+    },
+    rules: {
+      /* Angular */
+      "@angular-eslint/component-class-suffix": ["error", { suffixes: ["Component"] }],
+      "@angular-eslint/directive-class-suffix": ["error", { suffixes: ["Directive"] }],
+      "@angular-eslint/use-lifecycle-interface": "warn",
+
+      /* TypeScript */
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-floating-promises": "error",
+
+      /* Clean Code */
+      "no-console": "warn",
+      "no-debugger": "warn",
+      eqeqeq: ["error", "always"],
+      curly: ["error", "all"],
+
+      /* Imports */
+      "import/order": [
+        "error",
+        {
+          "newlines-between": "always",
+          groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+          alphabetize: { order: "asc", caseInsensitive: true }
+        }
+      ],
+      "import/no-unresolved": "off",
+
+      /* Documentação */
+      "jsdoc/require-jsdoc": [
+        "warn",
+        {
+          require: {
+            MethodDefinition: true,
+            ClassDeclaration: true,
+            FunctionDeclaration: true
+          }
+        }
+      ],
+      "jsdoc/require-param": "warn",
+      "jsdoc/require-returns": "warn"
+    }
+  },
+
+  // Angular HTML templates
+  {
+    files: ["**/*.html"],
+    plugins: {
+      "@angular-eslint/template": angularTemplate
+    },
+    rules: {
+      "@angular-eslint/template/no-negated-async": "error"
+    }
+  }
+];
